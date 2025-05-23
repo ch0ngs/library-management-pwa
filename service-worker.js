4d8cfb9e587e4786ad7956a5a3f6f29b7b48670c
ch0ngs <alifproduct123@gmail.com>
2025-05-24 00:36:22 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'library-ms-cache-v1';
+const urlsToCache = [
+  '.',
+  './library.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
